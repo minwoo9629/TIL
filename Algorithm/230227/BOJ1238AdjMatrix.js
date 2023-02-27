f94ab2b176722a7caaa4f1f209fb5df9eb95a70c
minwoo9629 <minwoo9629@gmail.com>
2023-02-27 17:07:03 +0900

    2023-02-27 백준 파티 인접 행렬 풀이

---
+++ Algorithm/230227/BOJ1238AdjMatrix.js
@@ -0,0 +1,114 @@
+const solution = () => {
+  let fs = require("fs");
+  let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+  const [N, M, X] = input[0].split(" ").map(Number);
+  const adjArr = Array.from(Array(N + 1), () => Array(N + 1).fill(0));
+  for (let i = 1; i <= M; i++) {
+    const [from, to, cost] = input[i].split(" ").map(Number);
+    adjArr[from][to] = cost;
+  }
+
+  let answer = 0;
+  const allDistance = {};
+  for (let start = 1; start <= N; start++) {
+    if (start !== X) {
+      const distance = dijkstra(start, adjArr, X);
+      allDistance[start] = distance;
+    }
+  }
+
+  for (let destination = 1; destination <= N; destination++) {
+    if (destination !== X) {
+      const distance = dijkstra(X, adjArr, destination);
+      allDistance[destination] += distance;
+      answer = Math.max(answer, allDistance[destination]);
+    }
+  }
+  return answer;
+};
+
+const dijkstra = (start, adjArr, destination) => {
+  const discovered = Array(adjArr.length).fill(Number.MAX_SAFE_INTEGER);
+  discovered[start] = 0;
+  const heap = new Heap();
+  heap.add([start, 0]);
+
+  while (heap.len !== 0) {
+    const [cityNum, distance] = heap.poll();
+    if (cityNum === destination) {
+      return distance;
+    }
+    for (let next = 1; next < adjArr.length; next++) {
+      if (adjArr[cityNum][next] === 0) {
+        continue;
+      }
+      const nextDistance = distance + adjArr[cityNum][next];
+      if (discovered[next] > nextDistance) {
+        discovered[next] = nextDistance;
+        heap.add([next, nextDistance]);
+      }
+    }
+  }
+};
+
+const Heap = function () {
+  this.heap = [null];
+  this.len = 0;
+  this.compare = function (idx1, idx2) {
+    return this.heap[idx1][1] > this.heap[idx2][1];
+  };
+  this.add = function (value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parentIdx = Math.floor(curIdx / 2);
+    while (curIdx > 1 && this.compare(parentIdx, curIdx)) {
+      this.swap(parentIdx, curIdx);
+      curIdx = parentIdx;
+      parentIdx = Math.floor(curIdx / 2);
+    }
+    this.len++;
+  };
+  this.swap = function (idx1, idx2) {
+    const temp = this.heap[idx1];
+    this.heap[idx1] = this.heap[idx2];
+    this.heap[idx2] = temp;
+  };
+
+  this.poll = function () {
+    const value = this.heap[1];
+    if (this.heap.length <= 2) {
+      this.heap = [null];
+      this.len = 0;
+    } else {
+      this.heap[1] = this.heap.pop();
+      this.len--;
+    }
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+    if (!this.heap[leftIdx]) {
+      return value;
+    }
+    if (!this.heap[rightIdx]) {
+      if (this.compare(curIdx, leftIdx)) {
+        this.swap(leftIdx, curIdx);
+      }
+      return value;
+    }
+
+    while (this.compare(curIdx, leftIdx) || this.compare(curIdx, rightIdx)) {
+      const minIdx = this.compare(leftIdx, rightIdx) ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+      if (leftIdx > this.len || rightIdx > this.len) {
+        break;
+      }
+    }
+
+    return value;
+  };
+};
+console.log(solution());
